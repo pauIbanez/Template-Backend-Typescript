0032793625d1a937e3649dc128add3f63a4c78fc
Pau Ibáñez <pauibanez2001@gmail.com>
2023-07-03 22:28:33 +0200

    commented the login controller

--- src/server/controllers/auth/login/login.ts
+++ src/server/controllers/auth/login/login.ts
@@ -17,13 +17,14 @@ import {
 } from "../../../../data/server-config";
 
 const login = async (req: Request, res: Response, next: NextFunction) => {
-  const loginData: LoginData = req.body;
+  const loginData: LoginData = req.body; // Grab the login data left in the body by the request validator.
 
   try {
     const foundUser: DatabaseUserData = await Users.findOne({
       "credentials.email": loginData.email,
-    });
+    }); // Get the user from the database by email.
 
+    // If no user matches the email write the error and go next.
     if (!foundUser) {
       const userNotFoundForEmailError = getUserNotFoundForEmailError(
         loginData.email
@@ -32,60 +33,69 @@ const login = async (req: Request, res: Response, next: NextFunction) => {
       return;
     }
 
+    // If the user is disabled write the error and go next.
     if (foundUser.isDisabled) {
       const userDisabledError = getUserDisabledError(foundUser.id);
       next(userDisabledError);
       return;
     }
 
-    const invalidPasswordError = getInvalidPasswordError(foundUser.id);
+    const invalidPasswordError = getInvalidPasswordError(foundUser.id); // Write the invalid password error.
 
     if (loginData.withOtp) {
+      // If the request is made with OTP and the DB user does not have one go next with the invalid password error written previously.
       if (!foundUser.credentials.otpPassword) {
         next(invalidPasswordError);
         return;
       }
 
+      // Check if the OTP is valid.
       const isPasswordValid = await bcrypt.compare(
         loginData.password,
         foundUser.credentials.otpPassword as string
       );
 
+      // If the password is invalid go next with the invalid password error written previously.
       if (!isPasswordValid) {
         next(invalidPasswordError);
         return;
       }
 
+      // If the password is valid erase the OTP (since it's, well, One time) and save the user back to the DB with the erases OTP.
       foundUser.credentials.otpPassword = "";
       foundUser.save();
     } else {
+      // If the  DB user does not have a password go next with the invalid password error written previously.
       if (!foundUser.credentials.password) {
         next(invalidPasswordError);
         return;
       }
 
+      // Check if the password is valid.
       const isPasswordValid = await bcrypt.compare(
         loginData.password.toString(),
         foundUser.credentials.password as string
       );
 
+      // If the  DB user does not have a password go next with the invalid password error written previously.
       if (!isPasswordValid) {
         next(invalidPasswordError);
         return;
       }
     }
-
+    // If every check is passed, write the payload with the id of the user and a token refresh time.
     const tokenData: TokenPayload = {
       id: foundUser.id,
       tokenRefreshTime: userSessionRefresh,
     };
 
+    // This created the token with the previously created payload.
     const newToken = jwt.sign(tokenData, process.env.TOKEN_SECRET, {
       expiresIn: userSessionDuration,
     });
-    res.json({ token: newToken });
+    res.json({ token: newToken }); // Send the token as the response.
   } catch (error) {
-    next(error);
+    next(error); // If anything throws and error, pass it on to the next function.
   }
 };
 
