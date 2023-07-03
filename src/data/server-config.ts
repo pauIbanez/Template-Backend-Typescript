4ee87b1bdd08b6fc7a647611a35ac4416c6d26dd
Pau Ibáñez <pauibanez2001@gmail.com>
2023-07-03 22:11:38 +0200

    commented the utils

--- src/data/server-config.ts
+++ src/data/server-config.ts
@@ -3,5 +3,13 @@
 
 // USER SETTINGS
 
+// Session settings
 export const userSessionDuration = "2h"; // Duration of the user session
 export const userSessionRefresh = "1h"; // Time before the front should refresh the token
+
+// Password settings
+export const charactersOTP =
+  "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+export const lengthOTP = 10;
+
+export const saltRounds = 10;
