464755c49eaf356296993ef30c5a3c9519b1d82f
Pau Ibáñez <pauibanez2001@gmail.com>
2023-07-03 22:31:28 +0200

    commented the loginDataValidator middleware

--- src/server/middlewares/requestPayloadValidators/loginDataValidator/loginDataValidator.ts
+++ src/server/middlewares/requestPayloadValidators/loginDataValidator/loginDataValidator.ts
@@ -7,14 +7,16 @@ const loginDataValidator = (
   res: Response,
   next: NextFunction
 ) => {
+  // Options to tell Joi how to validate the payload
   const joiValidationOptions = {
     abortEarly: false,
     allowUnknown: true,
     stripUnknown: true,
   };
 
-  const { error, value } = loginData.validate(req.body, joiValidationOptions);
+  const { error, value } = loginData.validate(req.body, joiValidationOptions); // Use the loginData Joi object to validate the request body. (This object is found in data/JoiObjects)
 
+  // If the validation fails write an error and go next.
   if (error) {
     const invalidLoginDataError = getInvalidLoginDataError(
       error.details.map((detail) => detail.message).join(", ")
@@ -22,6 +24,8 @@ const loginDataValidator = (
     next(invalidLoginDataError);
     return;
   }
+
+  // If the validation is correct we override the request body with the validated object and go next.
   req.body = value;
   next();
 };
