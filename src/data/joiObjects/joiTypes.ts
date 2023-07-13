f1139f00bce7b1c10eea340d82f11fdc2d5b2fa3
Pau Ibáñez <pauibanez2001@gmail.com>
2023-07-13 16:34:24 +0200

    removed com mented out code

--- src/data/joiObjects/joiTypes.ts
+++ src/data/joiObjects/joiTypes.ts
@@ -2,15 +2,5 @@
 
 import Joi from "joi";
 
-// export const JoiObjectId = (message?: string) =>
-//   Joi.alternatives(
-//     Joi.string()
-//       .regex(/^[0-9a-fA-F]{24}$/, message)
-//       .length(24),
-//     Joi.object().keys({
-//       _bsontype: Joi.allow("ObjectId"),
-//     })
-//   );
-
 export const JoiObjectId = (message?: string) =>
   Joi.string().regex(/^[0-9a-fA-F]{24}$/, message);
